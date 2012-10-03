cfd788419f1c1569187899b659eece226d4896a8
yamashita_tomonori <yamashita_tomonori@nexway.co.jp>
2012-10-03 14:34:31 +0900

    10日03日分_テストに合わせて変更

--- practice002.rb
+++ practice002.rb
@@ -3,65 +3,85 @@ require 'rspec'
 
 class Game
   def initialize
+    @throw_cnt = 0
+    @score_array = Array.new()
+    @current_frame = 1
     @score = 0
-    @throw_num = 0
   end
 
-  def throw(num)
-    increment_throw_num
-    if @throw_num % 3 == 0
-      if @score == 10
-        num = num * 2
-      end
+  def throw(cnt)
+    increment_throw_cnt()
+    @score_array << cnt
+    if @throw_cnt % 2 == 0
+      @current_frame += 1
     end
-    @score += num
   end
 
-  def increment_throw_num
-    @throw_num += 1
+  def increment_throw_cnt
+    @throw_cnt += 1
   end
+
+  def score
+    @score_array.each do |value|
+      @score += value
+  def current_frame
+    @current_frame
+  end
+end
+end
+
 
   def score
     @score
   end
 end
 
-unless __FILE__ == $0
-  describe Game do
-    it '一投目1点、2投目2点だったら3ptになる' do
-      game = Game.new()
+game = Game.new()
+game.throw(2)
+game.throw(3)
 
-      game.throw(1)
-      game.throw(2)
+game.score
 
-      game.score().should == 3
-    end
 
-    it '1フレーム目スペア、2フレーム目1投目5点だったら20ptになる' do
-      game = Game.new()
-
-      game.throw(5)
-      game.throw(5)
-
-      game.throw(5)
-      game.throw(0)
-
-      game.score().should == 20
-    end
-
-    it '1フレーム目スペア、2フレーム目0点だったら10ptになる' do
-      game = Game.new()
-
-      game.throw(5)
-      game.throw(5)
-
-      game.throw(0)
-      game.throw(0)
-
-      game.score().should == 10
-    end
-  end
-end
+#unless __FILE__ == $0
+#  describe Game do
+#    it '一投目1点、2投目2点だったら3ptになる' do
+#      game = Game.new()
+#
+#      game.throw(1)
+#      game.throw(2)
+#
+#      game.score().should == 3
+#    end
+#
+#    it '1フレーム目スペア、2フレーム目1投目5点だったら20ptになる' do
+#      game = Game.new()
+#
+#      game.throw(5)
+#      game.throw(5)
+#
+#      game.throw(5)
+#      game.throw(0)
+#
+#      game.score().should == 20
+#    end
+#
+#    it '1投ガーターたったら現在のフレームは1になること' do
+#      game = Game.new()
+#      game.throw(0)
+#
+#      game.current_frame.should == 1
+#    end
+#
+#    it '2投ガーターだったら現在のフレームは2になること' do
+#      game = Game.new()
+#      game.throw(0)
+#      game.throw(0)
+#
+#      game.current_frame.should == 1
+#    end
+#  end
+#end
 
 
 
