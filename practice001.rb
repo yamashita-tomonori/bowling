64026ccaf2c949d1a735d224195dff92912f1550
yamashita_tomonori <yamashita_tomonori@nexway.co.jp>
2012-10-03 13:04:55 +0900

    ボーリングの計算（スペア、ストライクあり）

--- practice001.rb
+++ practice001.rb
@@ -4,7 +4,7 @@ require 'rspec'
 class Game
   def initialize
     @throw_cnt = 0
-    @score = Array.new(3) do
+    @score = Array.new(4) do
       [0, 0]
     end
     @frame_score = Array.new
@@ -13,20 +13,49 @@ class Game
     input_score()
   end
 
+  #コマンドラインからスコアを入力する
   def input_score
-    for i in 0..2
+    for i in 0..3
       for j in 0..1
-        print i + 1, "フレーム目", j + 1 , "投目のスコアを入力してください："
-        @score[i][j] = gets.to_i
+        #ストライクの場合
+        if j == 1 && @score[i][0] == 10
+          print i + 1, "フレーム ", j + 1 , "投目のスコアを入力してください：0\n"
+          @score[i][j] = 0
+        #ストライク以外の場合
+        else
+          print i + 1, "フレーム ", j + 1 , "投目のスコアを入力してください："
+          @score[i][j] = gets.to_i
+        end
       end
     end
     score()
   end
 
+  #各フレームのスコア、合計スコアを計算
   def score
-    for i in 0..2
-      if @score[i][0] + @score[i][1] == 10
+    for i in 0..3
+      #ストライクの場合
+      if @score[i][0] == 10
+        #次がストライクでない場合
+        if @score[i + 1][0] != 10
+          @frame_score[i] = 10 + @score[i + 1][0] + @score[i + 1][1]
+        #次がストライクの場合
+        else
+          num = 1
+          str_num = i + 1
+          #連続するストライクの回数を計算
+          while @score[str_num][0] == 10
+            num += 1
+            str_num += 1
+          end
+          @frame_score[i] = num * 10 + @score[str_num][0]
+        end
+
+      #スペアの場合
+      elsif @score[i][0] + @score[i][1] == 10
         @frame_score[i] = 10 + @score[i+1][0]
+
+      #それ以外の場合
       else
         @frame_score[i] = @score[i][0] + @score[i][1]
       end
@@ -36,6 +65,7 @@ class Game
     print "合計スコア：", @score_sum, "\n"
   end
 end
+
 
 game = Game.new()
 
@@ -75,8 +105,3 @@ game = Game.new()
 #    end
 #  end
 #end
-
-
-
-
-
